02c1424093ebebe8797444af7befd76404f044c4
Koby <koby.outama@gmail.com>
2022-03-23 23:11:27 -0700

    Completed createCSV function

--- src/datproc.py
+++ src/datproc.py
@@ -9,40 +9,34 @@ import csv
 import helpers
 import pandas as pd
 from exceptions import NotImplementedError, InvalidDimensionError, InvalidWritePath
-from os import getcwd, listdir
-from os import remove as file_remove
-from os.path import exists as file_exists
+from os import getcwd, listdir, remove
+from os.path import exists, splitext
 
 
-class DataProc():
+class DataProc:
     HEADER = ["Date", "Description", "Cost"]
-    FILE_PATH =  getcwd() + '/../Out/fts_dat.csv'
-    REGULAR_EXPRESSION = r"^fts_dat(\(\d\))?.csv$"
+    FILE_PATH = getcwd() + '/../Out/fts_dat.csv'
+    REGULAR_EXPRESSION = r"^fts_dat(\(\d?\d?\))?.csv$"
 
-    def __init__(self) -> None:
-        pass
-
-    def createCSV(self, file=FILE_PATH, header=HEADER):
-        self.file = file
+    def __init__(self, file=FILE_PATH, header=HEADER) -> None:
+        self.file = helpers.parsefile(file)
         self.header = header
-        if not file_exists(self.file):
+
+    def createCSV(self):
+        if not exists(self.file):
             with open(self.file, 'w+', encoding='UTF8', newline='') as f:
                 writer = csv.writer(f)
                 writer.writerow(self.header)
         else:
-            # this path will create a new file with new added or subtracted headers
-            # self.file = self.file[:-3] + '(%d)'.format(len[]) + .csv
-            cf = len([f for f in listdir(path='../Out') if re.match(DataProc.REGULAR_EXPRESSION,f)])
-            self.file = getcwd() + f'/../Out/fts_dat({cf}).csv'
+            self.file = helpers.newFile(self.file)
             with open(self.file, 'w+', encoding='UTF8', newline='') as f:
                 writer = csv.writer(f)
                 writer.writerow(self.header)
-            # cf = len([f for f in listdir(path='../Out') if f.startswith('') and f.endswith('.csv')])
 
     def writeRow(self, row: []) -> int:
         if len(row) != len(self.header):
             raise InvalidDimensionError(f"Row Dimension of {len(row)} != Header Dimension of {len(self.header)}")
-        if file_exists(self.file) :
+        if exists(self.file) :
             with open(self.file, 'a', encoding='UTF8', newline='') as f:
                 writer = csv.writer(f)
                 writer.writerow([helpers.getTime()]+row)
@@ -51,8 +45,9 @@ class DataProc():
             raise InvalidWritePath("No working CSV file")
 
     def delCSV(self, path: str) -> int:
-        if path.endswith('.csv') and file_exists(path):
-            file_remove(path)
+        path = path if path.startswith(f"{getcwd()}/../Out") else f"{getcwd()}/../Out/{path}"
+        if path.endswith('.csv') and exists(path):
+            remove(path)
             return 1
         else:
             return 0
@@ -61,13 +56,25 @@ class DataProc():
         raise NotImplementedError("delRow not implemented")
 
     def editCSV(self, row: int, col: int, file):
-        if not file_exists(file):
+        if not exists(file):
             raise InvalidWritePath("No working CSV file")
         raise NotImplementedError("Edit CSV not implemented")
 
+
 if __name__ == "__main__":
-    print("DatProc")
-    s = DataProc()
     # ctr + / to chunk comment
-    s = DataProc()
-    s.delCSV(getcwd()+f"/../Out/fts_dat(10).csv")
+    print("DatProc")
+    # for x in listdir(getcwd()+'/../Out'):
+    #    print(DataProc.delCSV(DataProc, f'{getcwd()}/../Out/{x}'))
+    #    DataProc.delCSV(DataProc,x)
+    #
+    # s = DataProc("asd.csv")
+    # s.createCSV()
+    # s = DataProc("das")
+    # s.createCSV()
+    # s = DataProc(getcwd()+"/../Out/sad.csv")
+    # s.createCSV()
+    # s = DataProc("asd.csv")
+    # s.createCSV()
+    # s = DataProc()
+    # s.createCSV()
